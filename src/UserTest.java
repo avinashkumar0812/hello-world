956f6ce32e5ee686913264aa3c1d0988412bcb81
javacodemailtesting <46401148+javacodemailtesting@users.noreply.github.com>
2019-01-05 22:31:11 +0530

    Revert "User commit 3"
    This reverts commit ff1af5f343491145e8a54c08e44882518e36a452.

--- src/UserTest.java
+++ src/UserTest.java
@@ -6,7 +6,6 @@ public class UserTest {
 		System.out.println("User Test");
 		System.out.println("User Commit 1");
 		System.out.println("User Commit 2");
-		System.out.println("User Commit 3");
 		
 	}
 
