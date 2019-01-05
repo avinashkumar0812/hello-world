36b2bc75b3d3257e33fbb249e94a40df7aadca59
javacodemailtesting <46401148+javacodemailtesting@users.noreply.github.com>
2019-01-05 22:31:11 +0530

    Revert "User commit 4"
    This reverts commit 2d947c03ee4114345b44a3a9f21e3bb5c170280c.

--- src/UserTest.java
+++ src/UserTest.java
@@ -7,7 +7,7 @@ public class UserTest {
 		System.out.println("User Commit 1");
 		System.out.println("User Commit 2");
 		System.out.println("User Commit 3");
-		System.out.println("User Commit 4");
+		
 	}
 
 }
