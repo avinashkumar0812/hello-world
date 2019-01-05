bff06d8183ebc71e027fe2cb9abd6ff92ae9aa24
Avinash <avinashkumar0812@gmail.com>
2019-01-05 22:54:13 +0530

    COnsolidated message  for user-branch-2 coomits

--- src/UserTest.java
+++ src/UserTest.java
@@ -5,6 +5,10 @@ public class UserTest {
 		
 		System.out.println("User Test");
 		
+		System.out.println("User branch 2 commit 1");
+		System.out.println("User branch 2 commit 2");
+		System.out.println("User branch 2 commit 3");
+		System.out.println("User branch 2 commit 4");
 	}
 
 }
